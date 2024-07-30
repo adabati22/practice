aad5917b6dfbccf1c161eb43269deaff4649f2fe
adabati22 <adara2291@gmail.com>
2024-07-30 15:36:24 -0300

    archivos

---
+++ js/script.js
@@ -0,0 +1,77 @@
+// crear variables con los siguientes tipos de datos: string,number y booleano
+
+//  let cantidadDeMascotas = 1;
+//  let nombreDeTusMascotas = " bagheera";
+//  let mascotasExoticas = prompt ( " ¿tienes mascotas exoticas? " );
+// let cantidadDeAlimento = 2;
+// cantidadDeMascotas = 3;
+// let especie;
+// if( mascotasExoticas == "si" ){
+//   especie =  prompt ("escribe la especie");
+// }
+// else if(especie == "pantera"){
+//     alert ("creo que eres amante de los michis");
+// }
+// else if( especie =="iguana"){
+//     alert("eres amante de los reptiles");
+// }
+// else{
+//     alert ("gracias por responder");
+// }
+// let consumoDeMascotas = (cantidadDeMascotas * cantidadDeAlimento) ;
+
+
+//ejercicio 1
+// let cantidad = prompt('INGRESE CANTIDAD DE REPETICIONES');
+// let texto = prompt('INGRESE TEXTO A REPETIR');
+// for (let index = 0; index < cantidad; index ++) {
+// console.log(texto);
+// }
+// console.log("gracias por participar")
+
+
+//ejercicio 2
+// let lados = prompt('INGRESE CANTIDAD DE LADOS');
+// for (let index = 0; index < lados; index++) {
+// if (index < 3) {
+//     alert("lados");
+// }
+// break;
+// }
+
+
+//actividad 2
+// let numero = prompt ("ingrese un numero");
+// let resultado = numero % 2;
+// if( resultado == 0){
+//     alert("tu numero es par");
+// }
+// else{
+//     alert ("tu numero es impar");
+// }
+
+
+//actividad 3
+// let numero = prompt ("ingrese un numero");
+// let resultado = numero % 2;
+// while( resultado == 0){
+//     alert("tu numero es par");
+
+// mundial
+// let edad = prompt("ingrese su edad ");
+// let mundialesTotales = 0;
+// const primerMundial=1930;
+// const actual=2024;
+// for ( let i=primerMundial; i<= actual; i+=4){
+//     console.log(i)
+//     if(i > actual){
+//         break;
+//     }
+//     if(i == 1942 || i ==1946 || (actual-edad)>i){
+//          continue;
+//     }
+       
+//     mundialesTotales++;
+// }
+// alert("viviste" + mundialesTotales + "en tu vida")
+
